02ff7a56892eb5626db07edd993675071302c553
unknown <eliasjrcosta886@gmail.com>
2023-02-25 00:14:42 -0300

    Iniciando logica js
    Neste Commit, comecei a trabalhar na lógica do js. Criei as variávieis para receber os valores dos inputs, e troquei o evento 'click' da versão anterior pelo 'focus'. Também comecei a fazer a captura dos dados do input e imprimir no cartão !

--- cardEfect.js
+++ cardEfect.js
@@ -1,5 +1,25 @@
 const card = document.querySelector("#mainContainer")
+const numberCard = document.querySelector("#numberCard")
+const nameCard = document.querySelector("#nameCard")
+const cvv = document.querySelector('#cvvInputCard')
+const butSubmit = document.querySelector('#butsubmit')
+const valueNumber = document.querySelector('#valueNumber')
+const valueName = document.querySelector('#valueName')
+const validityDate = document.querySelector('#validityDate')
+const vDate = document.querySelector('#vDate')
+const cvvValueCard = document.querySelector('#cvvValueCard')
 
-card.addEventListener("click", (e)=>{
+cvv.addEventListener("focus", ()=>{
     card.classList.toggle("flip")
 })
+
+butSubmit.addEventListener("click", ()=>{
+    valueNumber.innerHTML = numberCard.value
+    valueName.innerHTML = nameCard.value
+    vDate.innerHTML = validityDate.value
+    cvvValueCard.innerHTML = cvv.value
+})
+
+
+
+
