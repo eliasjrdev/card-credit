8f41ba16236afe455e6cc1be26c48fef05903990
unknown <eliasjrcosta886@gmail.com>
2023-02-26 01:02:30 -0300

    Continuando parte lógica
    Neste commit, adicionei o evento Listener 'input' para atualizar de maneira dinâmica os dados na face do cartão ao mesmo tempo que os inputs vão sendo preenchidos.

--- cardEfect.js
+++ cardEfect.js
@@ -1,24 +1,56 @@
-const card = document.querySelector("#mainContainer")
-const numberCard = document.querySelector("#numberCard")
-const nameCard = document.querySelector("#nameCard")
-const cvv = document.querySelector('#cvvInputCard')
-const butSubmit = document.querySelector('#butsubmit')
-const valueNumber = document.querySelector('#valueNumber')
+// Elements of face card
+const card = document.querySelector('#mainContainer')
 const valueName = document.querySelector('#valueName')
-const validityDate = document.querySelector('#validityDate')
+const valueNumber = document.querySelector('#valueNumber')
 const vDate = document.querySelector('#vDate')
 const cvvValueCard = document.querySelector('#cvvValueCard')
 
-cvv.addEventListener("focus", ()=>{
-    card.classList.toggle("flip")
+//Elements of inputs
+const numberCard = document.querySelector("#numberCard")
+const nameCard = document.querySelector("#nameCard")
+const cvv = document.querySelector('#cvvInputCard')
+const validityDate = document.querySelector('#validityDate')
+const butSubmit = document.querySelector('#butsubmit')
+
+
+//----------------------------------------------------//
+
+numberCard.addEventListener('input', updateNumberCard);
+nameCard.addEventListener('input', updateNameCard);
+cvv.addEventListener('input', updateCvv);
+validityDate.addEventListener('input', updateValidityDate);
+
+
+function updateNumberCard(){
+    valueNumber.textContent = numberCard.value;
+}
+
+function updateNameCard(){
+    valueName.textContent = nameCard.value;
+}
+
+function updateCvv(){
+    cvvValueCard.textContent = cvv.value;
+}
+
+function updateValidityDate(){
+    vDate.textContent = validityDate.value
+}
+
+cvv.addEventListener('focus', ()=>{
+    card.classList.toggle('flip')
 })
 
+
+
+
+/*
 butSubmit.addEventListener("click", ()=>{
     valueNumber.innerHTML = numberCard.value
     valueName.innerHTML = nameCard.value
     vDate.innerHTML = validityDate.value
     cvvValueCard.innerHTML = cvv.value
-})
+})*/
 
 
 
